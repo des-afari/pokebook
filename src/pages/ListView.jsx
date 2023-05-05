7d42fd0c82c0f3c64ce6d95ab1d66893275aabcb
Desmond Afari <afaridesmond@gmail.com>
2023-05-05 17:04:02 +0000

    main page

--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,41 +1,33 @@
 import { useContext, useEffect, useState } from 'react'
 import '../static/css/listview.css'
+import { ReactComponent as View } from '../assets/svg/view.svg'
 import { AppContext } from '../App'
 import Header from '../components/Header'
 import axios from 'axios'
+import ReactPaginate from 'react-paginate'
 
 
 const ListView = () => {
-  const {theme} = useContext(AppContext)
+  const {theme, theme_1, theme_2, theme_3} = useContext(AppContext)
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
+  const [pageNumber, setPageNumber] = useState(0)
+  const [currentTheme, setCurrentTheme] = useState('')
+  const [hoverCard, setHoverCard] = useState(null)
 
   const typeEmojis = {
-    "normal": "🐻",
-    "fire": "🔥",
-    "water": "💧",
-    "electric": "⚡️",
-    "grass": "🍃",
-    "ice": "❄️",
-    "fighting": "🥊",
-    "poison": "☠️",
-    "ground": "🌍",
-    "flying": "🕊️",
-    "psychic": "🔮",
-    "bug": "🐛",
-    "rock": "🪨",
-    "ghost": "👻",
-    "dragon": "🐉",
-    "dark": "🌑",
-    "steel": "🔩",
-    "fairy": "🧚"
+    "normal": "🐻", "fire": "🔥", "water": "💧", "electric": "⚡️",
+    "grass": "🍃", "ice": "❄️", "fighting": "🥊", "poison": "☠️",
+    "ground": "🌍", "flying": "🕊️", "psychic": "🔮", "bug": "🐛",
+    "rock": "🪨", "ghost": "👻", "dragon": "🐉", "dark": "🌑",
+    "steel": "🔩", "fairy": "🧚"
   };
 
   useEffect(() => {
     const getPokemons = async () => {
       try{
         setLoading(true)
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=8')
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
         const pokemons = response.data.results
         const details = await Promise.all(pokemons.map(async item => {
           const res = await axios.get(item.url)
@@ -61,33 +53,74 @@ const ListView = () => {
     getPokemons()
   }, [])
 
+  useEffect(() => {
+    if (theme === theme_1) {
+      setCurrentTheme('theme_one')
+    } else if (theme === theme_2) {
+      setCurrentTheme('theme_two')
+    } else if (theme === theme_3) {
+      setCurrentTheme('theme_three')
+    }
+  }, [theme])
+
+  const itemsPerPage = 8
+  const pagesVisited = pageNumber * itemsPerPage
+
+  const displayPokemons = data.slice(pagesVisited, pagesVisited + itemsPerPage).map(
+    item => {
+      return (
+        <div key={item.id} className={`card ${hoverCard === item.id ? 'hovered' : ''}`}
+             onMouseEnter={() => setHoverCard(item.id)}
+             onMouseLeave={() => setHoverCard(null)}
+        >
+          <div>
+            <img src={item.img} alt="image" />
+          </div>
+          <div>
+            <p> {item.name} </p>
+            <div className='types_container'>
+              {item.pokemonTypes.map(type => (
+                <span key={type.name}>
+                    {type.emoji} {type.name}
+                </span>
+              ))
+            }</div>
+          </div>
+          <button className='view_button' style={{background: theme, color: 'white'}} onClick={() => console.log('click')}>
+            <span>View Pokemon</span>
+            <span><View /></span>
+          </button>
+        </div>
+      )
+    }
+  )
+
+  const pageCount = Math.ceil(data.length/itemsPerPage)
+  const pageChange = ({selected}) => {
+    setPageNumber(selected)
+  }
+
   return (
     <div className='listview'>
       <Header />
       <div className='listview_main'>
-          {
-            loading ? <p>loading...</p> : <div className='list_main_container'>
-              {data.map(item => (
-                <div key={item.id} className='card'>
-                  <div>
-                    <img src={item.img} alt="image" />
-                  </div>
-                  <div>
-                    <p> {item.name} </p>
-                    <div className='types_container'>
-                      {item.pokemonTypes.map(type => (
-                        <span key={type.name}>
-                            {type.emoji}
-                            {type.name}
-                        </span>
-                      ))
-                    }</div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          }
-        <div className='pagination'></div>
+        {
+          loading ? <p>Loading...</p> : <>
+          <div className='list_main_container'>
+            {displayPokemons}
+          </div>
+          <div className='pagination'>
+          <ReactPaginate
+            pageRangeDisplayed={4}
+            marginPagesDisplayed={1}
+            previousLabel={'<'} nextLabel={'>'}
+            pageCount={pageCount} onPageChange={pageChange}
+            containerClassName='paginationBttns'
+            activeClassName={currentTheme}
+            />
+          </div>
+        </>
+        }
       </div>
     </div>
   )
